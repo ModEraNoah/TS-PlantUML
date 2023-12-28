43f6418e71872650ed81a45d19f215a16f26b4c5
ModEraNoah <klauck02@outlook.com>
2023-12-28 17:19:24 +0100

    new way of thinking init

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,108 @@
-import fs from "fs";
+import fs from 'fs';
 
 function getFileContent(file: string) {
 	let contents;
-	try { contents = fs.readFileSync(file).toString() }
-	catch (e) {
-		console.error("error while reading file:", e);
-		contents = null
+	try {
+		contents = fs.readFileSync(file).toString();
+	} catch (e) {
+		console.error('error while reading file:', e);
+		contents = null;
 	}
-	return contents
+	return contents;
 }
 
-const fileContent = getFileContent("/home/noah/Dokumente/Airline/src/Airline.ts")
-if (fileContent) {
-	const res = fileContent
-	const arr = []
-	for (let i = 0; i < fileContent.length; i++) {
-		let k = res.indexOf("class", i)
-		if (k > 0) {
-			console.log("index of class:", k)
-			const innerClassStartIndex = res.indexOf("{", k)
-			const bracketArr = []
-			bracketArr.push(fileContent[innerClassStartIndex])
-			const classText = getInnerClass(fileContent, innerClassStartIndex + 1, bracketArr)
-			i = k
-			arr.push(classText)
+const fileContent = getFileContent('../Airline/src/Airline.ts');
+
+let counter: number = 0;
+let wordStart: number = 0;
+let wordEnd: number = 0;
+let lastWord: string = '';
+console.log('file-length:', fileContent!.length);
+while (counter < fileContent!.length) {
+	if (isIndentation(fileContent!, counter)) {
+		if (wordStart < wordEnd) {
+			lastWord = handleWord(lastWord);
 		}
+		counter++;
+		wordStart = counter;
+	} else {
+		counter++;
+		wordEnd = counter;
 	}
-	console.log(arr)
-
-	const classClosing = arr[0].lastIndexOf("}")
-	const methods: any = []
-	for (let i = 0; i < classClosing - 1; i++) {
-		i = getClassMethods(i, arr[0], methods)
-	}
-	console.log("methods: ", methods)
 }
 
-function getInnerClass(text: string, startIndex: number, bracketArr: string[]): string {
-	let index = startIndex
-	while (bracketArr.length > 0) {
-		if (text[index] === "{") {
-			bracketArr.push("{")
-		} else if (text[index] === "}") {
-			bracketArr.pop()
+function isIndentation(s: string, index: number) {
+	return (
+		s[index] === ' ' ||
+		s[index] === '\n' ||
+		s[index] === '\r' ||
+		s[index] === '\r\n'
+	);
+}
+
+function handleWord(lastWord: string): string {
+	const currentWord = fileContent!.substring(wordStart, wordEnd);
+	if (currentWord === 'import') {
+		counter = Math.min(
+			fileContent!.indexOf('\n', counter),
+			fileContent!.indexOf(';', counter),
+		);
+	} else if (currentWord === 'export') {
+		// do nothing
+	}
+	// single-line comment
+	else if (currentWord === '//') {
+		counter = fileContent!.indexOf('\n', counter);
+	}
+	// multi-line comment
+	else if (currentWord === '/*') {
+		counter = fileContent!.indexOf('*/');
+	}
+	// -> if current Word points to a function/method
+	else if (
+		(currentWord!.includes('(') || currentWord === '(') &&
+		currentWord !== 'Error'
+	) {
+		if (currentWord === '(') {
+			console.log(
+				lastWord +
+				fileContent!
+					.substring(
+						counter - currentWord.length,
+						Math.min(
+							fileContent!.indexOf('{', counter - currentWord.length),
+							fileContent!.indexOf(';', counter - currentWord.length),
+						),
+					)
+					.replace(/\n/g, '')
+					.replace(/ /g, ''),
+			);
+			counter = Math.min(
+				fileContent!.indexOf('{', counter - currentWord.length),
+				fileContent!.indexOf(';', counter - currentWord.length),
+			);
+		} else if (currentWord!.includes('(')) {
+			const newCounter = Math.min(
+				Math.max(
+					fileContent!.indexOf('{', counter - currentWord.length),
+					counter,
+				),
+				Math.max(
+					fileContent!.indexOf(';', counter - currentWord.length),
+					counter,
+				),
+			);
+			console.log(
+				fileContent!
+					.substring(counter - currentWord.length, newCounter)
+					.trim()
+					.replace(/\n/g, '')
+					.replace(/ /g, ''),
+			);
+			counter = newCounter;
 		}
-
-		index++
+	} else {
+		console.log(currentWord);
 	}
-	return text.substring(startIndex, index)
+	return currentWord;
 }
-
-function getClassMethods(startIndex: number, innerClassText: string, methodsArr: {}[]): number {
-	let index = startIndex;
-	let paramsStartIndex = innerClassText.indexOf("(", index)
-	let paramsEndIndex = innerClassText.indexOf(")", paramsStartIndex)
-	let methodParams = innerClassText.substring(paramsStartIndex, paramsEndIndex + 1).replace(/\n/g, "").replace(/ /g, "").replace(/,/g, ", ")
-
-	const returnTypeIndex = innerClassText.indexOf(":", paramsEndIndex)
-	const nextBracketStartIndex = innerClassText.indexOf("{", paramsEndIndex)
-	let returnType: string | null = null 
-	if (returnTypeIndex > 0 && nextBracketStartIndex > returnTypeIndex) {
-		returnType = innerClassText.substring(returnTypeIndex + 1,nextBracketStartIndex ).trim()
-	}
-
-	const preParamsString = innerClassText.substring(index, paramsStartIndex).trimEnd()
-	const paramsName = preParamsString.substring(preParamsString.lastIndexOf(" ") + 1)
-
-	if (paramsName && methodParams) {
-		//console.log({ name: paramsName, params: methodParams })
-		methodsArr.push({ name: paramsName, params: methodParams, returnType: returnType })
-	}
-
-	index = innerClassText.indexOf("{", paramsEndIndex) + 1
-	const bracketArr = ["{"]
-	while (bracketArr.length > 0) {
-		if (innerClassText[index] === "{") {
-			bracketArr.push("{")
-		} else if (innerClassText[index] === "}") {
-			bracketArr.pop()
-		}
-
-		index++
-	}
-
-	return index
-}
-
